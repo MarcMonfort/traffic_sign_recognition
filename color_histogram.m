edf38a66a8ba24a658c9bf518d0fe15863b0aa14
Marc Monfort <32013555+MarcMonfort@users.noreply.github.com>
2021-01-02 12:23:56 +0100

    color histogram features

--- color_histogram.m
+++ color_histogram.m
@@ -1,48 +1,30 @@
 clc
 close all
 
+I = imread('00012_00001_00001.png');
+figure; imshow(I)
 
-image = imread("00014_00002_00029.png");
-imshow(image)
+hsv = rgb2hsv(I);
+h = hsv(:, :, 1);
+s = hsv(:, :, 2);
+v = hsv(:, :, 3);
 
-%Split into RGB Channels
-Red = image(:,:,1);
-Green = image(:,:,2);
-Blue = image(:,:,3);
-%Get histValues for each channel
-[yRed, x] = imhist(Red);
-[yGreen, x] = imhist(Green);
-[yBlue, x] = imhist(Blue);
-%Plot them together in one plot
-plot(x, yRed, 'Red', x, yGreen, 'Green', x, yBlue, 'Blue');
+RED = (h < 15/360 | h > 345/360) & (s > 0.3);
+BLUE = (h > 200/360 & h < 230/360) & (s > 0.5);
+YELLOW = (h > 25/360 & h < 50/360) & (s > 0.5);
 
-rgbImage = imread('00040_00009_00029.png');
-hsvImage = rgb2hsv(rgbImage);
-hImage = hsvImage(:, :, 1);
-sImage = hsvImage(:, :, 2);
-vImage = hsvImage(:, :, 3);
-figure;
-subplot(2,2,1);
-hHist = histogram(hImage);
-grid on;
-title('Hue Histogram');
-subplot(2,2,2);
-sHist = histogram(sImage);
-grid on;
-title('Saturation Histogram');
-subplot(2,2,3);
-vHist = histogram(vImage);
-grid on;
-title('Value Histogram');
-subplot(2,2,4);
-imshow(rgbImage);
+BW = BLUE;
 
 
-figure, imhist(rgbImage)
+numPixel = numel(BLUE)
 
-x = imhist(rgbImage)
+pRed = sum(RED(:) / numPixel)
+pBlue = sum(BLUE(:) / numPixel)
+pYellow = sum(YELLOW(:) / numPixel)
 
-I = imsegkmeans(rgbImage, 2)
-imshow(I)
 
+
+%GOD = imerode(GOD, strel('square',2));
+
+figure; imshow(BW)
 
