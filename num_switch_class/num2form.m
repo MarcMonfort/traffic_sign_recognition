cfc969412b04cffc584c9e3ecf2522bf19306efd
Marc <marc.monfort@gmail.com>
2021-01-08 12:12:30 +0100

    new read images code

---
+++ num_switch_class/num2form.m
@@ -0,0 +1,92 @@
+function [class] = num2form(num)
+    switch num
+        case 0
+            class = "Circular";
+        case 1
+            class = "Circular";
+        case 2
+            class = "Circular";
+        case 3
+            class = "Circular";
+        case 4
+            class = "Circular";
+        case 5
+            class = "Circular";
+        case 6
+            class = "Circular";
+        case 7
+            class = "Circular";
+        case 8
+            class = "Circular";
+        case 9
+            class = "Circular";
+        case 10
+            class = "Circular";
+        case 11
+            class = "Triangular";
+        case 12
+            class = "Rombe";
+        case 13
+            class = "Triangular-invertit";
+        case 14
+            class = "Octagonal";
+        case 15
+            class = "Circular";
+        case 16
+            class = "Circular";
+        case 17
+            class = "Circular";
+        case 18
+            class = "Triangular";
+        case 19
+            class = "Triangular";
+        case 20
+            class = "Triangular";
+        case 21
+            class = "Triangular";
+        case 22
+            class = "Triangular";
+        case 23
+            class = "Triangular";
+        case 24
+            class = "Triangular";
+        case 25
+            class = "Triangular";
+        case 26
+            class = "Triangular";
+        case 27
+            class = "Triangular";
+        case 28
+            class = "Triangular";
+        case 29
+            class = "Triangular";
+        case 30
+            class = "Triangular";
+        case 31
+            class = "Triangular";
+        case 32
+            class = "Circular";
+        case 33
+            class = "Circular";
+        case 34
+            class = "Circular";
+        case 35
+            class = "Circular";
+        case 36
+            class = "Circular";
+        case 37
+            class = "Circular";
+        case 38
+            class = "Circular";
+        case 39
+            class = "Circular";
+        case 40
+            class = "Circular";
+        case 41
+            class = "Circular";
+        case 42
+            class = "Circular";
+
+
+    end
+end
